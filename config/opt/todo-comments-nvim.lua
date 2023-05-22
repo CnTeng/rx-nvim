b96d9b8e3bfe211ef7cdb4eb9f0633d68ffa4012
CnTeng <rxsnakepi@gmail.com>
2023-05-22 14:17:20 +0800

    feat: add google cal support

--- config/opt/todo-comments-nvim.lua
+++ config/opt/todo-comments-nvim.lua
@@ -1,7 +1,3 @@
-vim.api.nvim_create_autocmd({ "BufRead" }, {
-  callback = function() require("todo-comments").setup {} end,
-})
-
 local keys = {
   { "n", "]t", function() require("todo-comments").jump_next() end, { desc = "Next todo comment" } },
   { "n", "[t", function() require("todo-comments").jump_prev() end, { desc = "Previous todo comment" } },
@@ -11,6 +7,9 @@ local keys = {
 require("core.utils").lazy_plugin {
   events = "VimEnter",
   callback = function()
+    vim.api.nvim_create_autocmd({ "BufRead" }, {
+      callback = function() require("todo-comments").setup {} end,
+    })
     vim.cmd.packadd "todo-comments.nvim"
     require("core.utils").load_plugin { name = "todo-comments", keys = keys }
   end,
