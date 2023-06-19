fc1d388f042aebb7c363269e4d708cbf40964ee2
CnTeng <istengyf@outlook.com>
2023-06-19 19:26:02 +0800

    feat: change fmt

--- devshell/default.nix
+++ devshell/default.nix
@@ -1,17 +1,22 @@
-{ inputs, ... }: {
-  imports = map (n: inputs.${n}.flakeModule) [ "devshell" "treefmt-nix" ];
+{inputs, ...}: {
+  imports = map (n: inputs.${n}.flakeModule) ["devshell" "treefmt-nix"];
 
-  perSystem = { pkgs, system, config, ... }: {
-    _module.args.pkgs = import inputs.nixpkgs { inherit system; };
+  perSystem = {
+    pkgs,
+    system,
+    config,
+    ...
+  }: {
+    _module.args.pkgs = import inputs.nixpkgs {inherit system;};
 
     legacyPackages = pkgs;
 
-    devshells.default.packages = [ config.packages.default ];
+    devshells.default.packages = [config.packages.default];
 
     treefmt = {
       projectRootFile = "flake.nix";
       programs = {
-        nixfmt.enable = true;
+        alejandra.enable = true;
         prettier.enable = true;
         shfmt.enable = true;
         stylua.enable = true;
