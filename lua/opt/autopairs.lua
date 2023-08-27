3761500a6ad67de35438fd135a474f2cbd489740
CnTeng <rxsnakepi@gmail.com>
2023-08-27 22:38:38 +0800

    feat: add guess-indent

---
+++ lua/opt/autopairs.lua
@@ -0,0 +1,26 @@
+local opts = {
+  check_ts = true,
+  fast_wrap = {
+    map = "<M-e>",
+    chars = { "{", "[", "(", '"', "'" },
+    pattern = [=[[%'%"%>%]%)%}%,]]=],
+    end_key = "$",
+    keys = "qwertyuiopzxcvbnmasdfghjkl",
+    check_comma = true,
+    manual_position = true,
+    highlight = "Search",
+    highlight_grey = "Comment",
+  },
+}
+
+require("utils.plugin").lazy {
+  event = { "BufReadPre", "BufNewFile" },
+  pname = "nvim-autopairs",
+  name = "nvim-autopairs",
+  opts = opts,
+  config = function()
+    local cmp_autopairs = require "nvim-autopairs.completion.cmp"
+    local cmp = require "cmp"
+    cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+  end,
+}
