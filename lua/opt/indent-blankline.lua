966627e19e48e0e3dd19b2256f02e2f54d46a95d
CnTeng <me@snakepi.xyz>
2023-10-04 11:36:43 +0800

    feat: update indent blankline

--- lua/opt/indent-blankline.lua
+++ lua/opt/indent-blankline.lua
@@ -1,21 +1,12 @@
 local opts = {
-  char = "▎",
-  context_char = "▎",
-  use_treesitter = true,
-  filetype_exclude = {
-    "lspinfo",
-    "checkhealth",
-    "help",
-    "man",
-    "alpha",
-    "neo-tree",
+  exclude = {
+    filetypes = { "alpha", "neo-tree" },
   },
-  show_current_context = true,
 }
 
 require("utils").lazy {
   event = { "BufReadPre", "BufNewFile" },
   pack = "indent-blankline.nvim",
-  name = "indent_blankline",
+  name = "ibl",
   opts = opts,
 }
