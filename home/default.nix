28a6266c72e49e4145b9dca8e5f1b5f07c04c1b1
CnTeng <me@snakepi.xyz>
2023-11-28 13:58:51 +0800

    feat: use nixfmt instead of alejandra

--- home/default.nix
+++ home/default.nix
@@ -1,41 +1,42 @@
-self: {
-  lib,
-  config,
-  pkgs,
-  ...
-}:
-with lib; let
+self:
+{ lib, config, pkgs, ... }:
+with lib;
+let
   cfg = config.programs.rx-nvim;
+
   rx-nvim = self.packages.${pkgs.system}.default.override {
-    inherit (cfg) extraPackages;
-    gptSupport = cfg.gptSupport.enable;
-    gptHost = cfg.gptSupport.secrets.hostPath;
-    gptKey = cfg.gptSupport.secrets.keyPath;
+    inherit (cfg) extraPackages extraConfig;
   };
 in {
   options.programs.rx-nvim = {
     enable = mkEnableOption "rx-nvim";
-    defaultEditor = mkEnableOption "neovim as default editor" // {default = true;};
+    defaultEditor = mkEnableOption "neovim as default editor" // {
+      default = true;
+    };
     extraPackages = mkOption {
       type = with types; listOf package;
-      default = [];
+      default = [ ];
       description = "Extra packages available to nvim.";
     };
-    gptSupport = {
-      enable = mkEnableOption "ChatGPT support";
-      secrets = {
-        hostPath = mkOption {
-          type = types.str;
-        };
-        keyPath = mkOption {
-          type = types.str;
-        };
-      };
+    extraConfig = mkOption {
+      type = types.lines;
+      default = "";
+      description = "Extra configuration appended to nvim.";
     };
   };
 
   config = mkIf cfg.enable {
-    home.packages = [rx-nvim];
-    home.sessionVariables = mkIf cfg.defaultEditor {EDITOR = "nvim";};
+    home.packages = [ rx-nvim ];
+    home.sessionVariables = mkIf cfg.defaultEditor { EDITOR = "nvim"; };
+
+    xdg.desktopEntries.nvim-kitty = {
+      exec = getExe pkgs.kitty + " -e nvim %F";
+      icon = "nvim";
+      comment = "Edit text files";
+      terminal = false;
+      name = "Neovim Kitty";
+      genericName = "Text Editor";
+      categories = [ "Utility" "TextEditor" ];
+    };
   };
 }
