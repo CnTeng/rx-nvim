21056809ee96a8f7aa493c0dd135ae601644f2a9
CnTeng <rxsnakepi@gmail.com>
2024-05-06 10:10:14 +0800

    chore: disable builtin comment

--- config/lua/coding/comment.lua
+++ config/lua/coding/comment.lua
@@ -1,6 +1,11 @@
 return {
   "numToStr/Comment.nvim",
   name = "comment.nvim",
+  init = function() vim.keymap.del("n", "gcc") end,
   event = "LazyFile",
+  keys = {
+    { "gc", mode = { "n", "v" }, desc = "Comment toggle linewise" },
+    { "gb", mode = { "n", "v" }, desc = "Comment toggle blockwise" },
+  },
   opts = {},
 }
