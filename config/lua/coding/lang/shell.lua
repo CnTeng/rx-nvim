60f5e77ce5becce2b768048637ecf29a1dfd3b94
CnTeng <rxsnakepi@gmail.com>
2024-07-03 11:14:15 +0800

    feat: use bashls shfmt config

--- config/lua/coding/lang/shell.lua
+++ config/lua/coding/lang/shell.lua
@@ -1,14 +1,8 @@
 return {
-  {
-    "neovim/nvim-lspconfig",
-    opts = {
-      servers = { bashls = {} },
-    },
-  },
-  {
-    "stevearc/conform.nvim",
-    opts = {
-      formatters_by_ft = { sh = { "shfmt" } },
+  "neovim/nvim-lspconfig",
+  opts = {
+    servers = {
+      bashls = {},
     },
   },
 }
