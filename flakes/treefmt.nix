477d8855fd82320fdf9b5f9c9ac49d651ae9b7bb
CnTeng <rxsnakepi@gmail.com>
2024-07-09 23:49:34 +0800

    ci: clean flakes

--- flakes/treefmt.nix
+++ flakes/treefmt.nix
@@ -2,20 +2,18 @@
 {
   imports = [ inputs.treefmt.flakeModule ];
 
-  perSystem =
-    { pkgs, ... }:
-    {
-      treefmt = {
-        projectRootFile = "flake.nix";
+  perSystem = {
+    treefmt = {
+      projectRootFile = "flake.nix";
 
-        programs = {
-          nixfmt.enable = true;
-          prettier.enable = true;
-          stylua.enable = true;
-          taplo.enable = true;
-        };
-
-        settings.global.excludes = [ "pkgs/_sources/*" ];
+      programs = {
+        nixfmt.enable = true;
+        prettier.enable = true;
+        stylua.enable = true;
+        taplo.enable = true;
       };
+
+      settings.global.excludes = [ "pkgs/_sources/*" ];
     };
+  };
 }
