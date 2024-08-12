e341a5be7db8814af8ebc96f2a6cc440b6c36058
CnTeng <rxsnakepi@gmail.com>
2024-08-12 16:16:02 +0800

    feat: add quicker.nvim config

--- config/lua/ui/bqf.lua
+++ config/lua/ui/bqf.lua
@@ -1,6 +1,18 @@
 return {
-  "kevinhwang91/nvim-bqf",
-  dependencies = { "junegunn/fzf" },
-  ft = "qf",
-  opts = { auto_resize_height = true },
+  {
+
+    "kevinhwang91/nvim-bqf",
+    dependencies = { "junegunn/fzf" },
+    ft = "qf",
+    opts = {
+      auto_resize_height = true,
+      preview = {
+        border = "none",
+      },
+    },
+  },
+  {
+    "stevearc/quicker.nvim",
+    opts = {},
+  },
 }
