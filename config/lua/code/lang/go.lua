047183a0b98794ec7e244e8f6146946b39661b5e
CnTeng <rxsnakepi@gmail.com>
2024-10-09 11:03:46 +0800

    feat: add args for go main.go dap

--- config/lua/code/lang/go.lua
+++ config/lua/code/lang/go.lua
@@ -36,6 +36,15 @@ return {
           name = "Debug main.go",
           request = "launch",
           program = "${workspaceFolder}/main.go",
+          args = function()
+            return coroutine.create(function(dap_run_co)
+              local args = {}
+              vim.ui.input({ prompt = "Args: " }, function(input)
+                args = vim.split(input or "", " ")
+                coroutine.resume(dap_run_co, args)
+              end)
+            end)
+          end,
         },
       },
     },
