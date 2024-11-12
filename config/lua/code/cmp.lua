affefdf9062fded2e5a180f073fb6c0a2373b202
CnTeng <rxsnakepi@gmail.com>
2024-11-13 00:05:52 +0800

    feat: use blink.cmp instead of nvim-cmp

--- config/lua/code/cmp.lua
+++ config/lua/code/cmp.lua
@@ -1,91 +1,38 @@
 ---@type LazyPluginSpec
 return {
-  "hrsh7th/nvim-cmp",
-  dependencies = {
-    "hrsh7th/cmp-nvim-lsp",
-    "hrsh7th/cmp-buffer",
-    "hrsh7th/cmp-path",
-    "hrsh7th/cmp-cmdline",
-    "hrsh7th/cmp-emoji",
-    "onsails/lspkind.nvim",
-    {
-      "garymjr/nvim-snippets",
-      dependencies = "rafamadriz/friendly-snippets",
-      opts = { friendly_snippets = true },
+  "saghen/blink.cmp",
+  dependencies = "rafamadriz/friendly-snippets",
+  event = "InsertEnter",
+  opts = {
+    keymap = { preset = "enter" },
+    completion = {
+      list = {
+        selection = function(ctx)
+          return ctx.mode == "cmdline" and "auto_insert" or "preselect"
+        end,
+      },
+      documentation = {
+        auto_show = true,
+        auto_show_delay_ms = 0,
+        update_delay_ms = 0,
+      },
+    },
+    signature = { enabled = true },
+    sources = {
+      default = {
+        "lazydev",
+        "lsp",
+        "path",
+        "snippets",
+        "buffer",
+      },
+      providers = {
+        lazydev = {
+          name = "LazyDev",
+          module = "lazydev.integrations.blink",
+          score_offset = 100,
+        },
+      },
     },
   },
-  event = "VeryLazy",
-  opts = function()
-    local cmp = require("cmp")
-
-    return {
-      completion = {
-        completeopt = vim.o.completeopt,
-      },
-      formatting = {
-        format = require("lspkind").cmp_format({
-          maxwidth = 50,
-          ellipsis_char = "...",
-        }),
-      },
-      mapping = cmp.mapping.preset.insert({
-        ["<C-b>"] = cmp.mapping(cmp.mapping.scroll_docs(-4), { "i", "c" }),
-        ["<C-f>"] = cmp.mapping(cmp.mapping.scroll_docs(4), { "i", "c" }),
-        ["<CR>"] = cmp.mapping(cmp.mapping.confirm(), { "i", "c" }),
-        ["<Tab>"] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            cmp.select_next_item()
-          elseif vim.snippet.active({ direction = 1 }) then
-            vim.snippet.jump(1)
-          else
-            fallback()
-          end
-        end),
-        ["<S-Tab>"] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            cmp.select_prev_item()
-          elseif vim.snippet.active({ direction = 1 }) then
-            vim.snippet.jump(-1)
-          else
-            fallback()
-          end
-        end),
-      }),
-      sources = cmp.config.sources({
-        { name = "nvim_lsp" },
-        { name = "snippets" },
-        { name = "path" },
-      }, {
-        { name = "buffer" },
-        { name = "emoji" },
-      }),
-    }
-  end,
-  config = function(_, opts)
-    local cmp = require("cmp")
-    cmp.setup(opts)
-
-    cmp.setup.cmdline({ "/", "?" }, {
-      completion = {
-        completeopt = "menu,menuone,noselect",
-      },
-      mapping = cmp.mapping.preset.cmdline(),
-      sources = {
-        { name = "buffer" },
-      },
-    })
-
-    cmp.setup.cmdline(":", {
-      completion = {
-        completeopt = "menu,menuone,noselect",
-      },
-      mapping = cmp.mapping.preset.cmdline(),
-      sources = cmp.config.sources({
-        { name = "path" },
-        { name = "cmdline" },
-      }, {
-        { name = "buffer" },
-      }),
-    })
-  end,
 }
