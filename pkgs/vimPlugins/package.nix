d33f13120f159537d270cbf3f01d9044395ebba4
CnTeng <rxsnakepi@gmail.com>
2024-12-28 11:38:21 +0800

    fix: add check for session_manager

--- pkgs/vimPlugins/package.nix
+++ pkgs/vimPlugins/package.nix
@@ -3,7 +3,10 @@ prev.vimPlugins.extend (
   _: _: {
     copilot-status-nvim = mkPlugin "copilot-status-nvim" "copilot-status.nvim";
     kitty-scrollback-nvim = mkPlugin "kitty-scrollback-nvim" "kitty-scrollback.nvim";
-    neovim-session-manager = mkPlugin "neovim-session-manager" "neovim-session-manager";
+    neovim-session-manager = mkPlugin "neovim-session-manager" "neovim-session-manager" // {
+      dependencies = [ prev.vimPlugins.plenary-nvim ];
+      nvimRequireCheck = "session_manager";
+    };
     quicker-nvim = mkPlugin "quicker-nvim" "quicker.nvim";
   }
 )
