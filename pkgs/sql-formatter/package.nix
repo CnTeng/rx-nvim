272430af560bf9e6126d7bbd2b6004ec5683c9e1
CnTeng <rxsnakepi@gmail.com>
2025-01-19 13:26:08 +0800

    bump: sql-formatter

--- pkgs/sql-formatter/package.nix
+++ pkgs/sql-formatter/package.nix
@@ -4,17 +4,17 @@
   fetchFromGitHub,
 }:
 prev.sql-formatter.overrideAttrs rec {
-  version = "15.4.5";
+  version = "15.4.9";
 
   src = fetchFromGitHub {
     owner = "sql-formatter-org";
     repo = "sql-formatter";
     rev = "v${version}";
-    hash = "sha256-tuui3otjOyoNmAG8S+o/cFI7UJRNWtzMTf9S72hiRZM=";
+    hash = "sha256-rCM/RDyWGBAobsiODIujkcjkqM4uSFPdRfvPqgIJQaY=";
   };
 
   yarnOfflineCache = fetchYarnDeps {
     yarnLock = "${src}/yarn.lock";
-    hash = "sha256-R3PDDWxNtPK18adtHB4Jjp451Mp2p+5Fw6A1xkC58oY=";
+    hash = "sha256-w+riBFsxqQ3BY2MF5yih1www+TMChneuTOS+sKWmlus=";
   };
 }
