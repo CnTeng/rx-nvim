3ccb656af679c4f52dd33f8aa85d192d8d65bd1d
CnTeng <rxsnakepi@gmail.com>
2025-10-21 00:33:44 +0800

    chore(nix): drop flake-parts and git-hooks-nix

--- flake.nix
+++ flake.nix
@@ -1,70 +1,71 @@
 {
   description = "A custom Neovim configuration for Nix users.";
 
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-
-    flake-parts.url = "github:hercules-ci/flake-parts";
-
-    git-hooks-nix = {
-      url = "github:cachix/git-hooks.nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-  };
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
   outputs =
-    inputs@{ self, flake-parts, ... }:
-    flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = [
-        "x86_64-linux"
-        "aarch64-linux"
-      ];
-
-      imports = [
-        inputs.git-hooks-nix.flakeModule
-        ./modules
-        ./pkgs
-      ];
-
-      perSystem =
-        { config, pkgs, ... }:
-        {
-          devShells.default = pkgs.mkShell {
-            packages = [ pkgs.nvfetcher ];
-            shellHook = config.pre-commit.installationScript;
-          };
-
-          checks.rx-nvim = pkgs.nixosTest {
-            name = "rx-nvim";
-            nodes.machine = {
-              imports = [ self.nixosModules.default ];
-              programs.rx-nvim.enable = true;
-            };
-
-            testScript = ''
-              machine.wait_for_unit("default.target")
-              machine.succeed("which nvim")
-            '';
-          };
-
-          formatter = pkgs.nixfmt-tree.override {
-            settings = {
-              formatter.stylua = {
-                command = "stylua";
-                includes = [ "*.lua" ];
-              };
-              global.excludes = [ "pkgs/_sources/*" ];
-            };
-            runtimeInputs = [ pkgs.stylua ];
-          };
-
-          pre-commit.settings.hooks = {
-            treefmt = {
-              enable = true;
-              package = config.formatter;
-            };
-            commitizen.enable = true;
-          };
+    { self, nixpkgs, ... }:
+    let
+      mkPkgs =
+        system:
+        import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+          overlays = [ self.overlays.default ];
         };
+
+      forEachSystem =
+        f:
+        nixpkgs.lib.genAttrs [
+          "x86_64-linux"
+          "aarch64-linux"
+        ] (system: f (mkPkgs system));
+    in
+    {
+      overlays.default = import ./pkgs { inherit (nixpkgs) lib; };
+
+      packages = forEachSystem (pkgs: {
+        default = pkgs.rx-nvim;
+      });
+
+      nixosModules.default = {
+        nixpkgs.overlays = [ self.overlays.default ];
+        imports = [ ./modules ];
+      };
+
+      devShells = forEachSystem (pkgs: {
+        default = pkgs.mkShell {
+          packages = [ pkgs.nvfetcher ];
+        };
+      });
+
+      checks = forEachSystem (pkgs: {
+        rx-nvim = pkgs.nixosTest {
+          name = "rx-nvim";
+          nodes.machine = {
+            imports = [ self.nixosModules.default ];
+            programs.rx-nvim.enable = true;
+          };
+
+          testScript = ''
+            machine.wait_for_unit("default.target")
+            machine.succeed("which nvim")
+          '';
+        };
+      });
+
+      formatter = forEachSystem (
+        pkgs:
+        pkgs.nixfmt-tree.override {
+          settings = {
+            formatter.stylua = {
+              command = "stylua";
+              includes = [ "*.lua" ];
+            };
+            global.excludes = [ "pkgs/_sources/*" ];
+          };
+          runtimeInputs = [ pkgs.stylua ];
+        }
+      );
     };
 }
--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,32 +1,12 @@
-{
-  inputs,
-  lib,
-  self,
-  ...
-}:
-{
-  flake.overlays.default =
-    final: prev:
-    lib.packagesFromDirectoryRecursive {
-      callPackage = lib.callPackageWith (
-        prev.pkgs
-        // {
-          inherit prev;
-          sources = final.callPackage ./_sources/generated.nix { };
-        }
-      );
-      directory = ./.;
-    };
-
-  perSystem =
-    { pkgs, system, ... }:
-    {
-      _module.args.pkgs = import inputs.nixpkgs {
-        inherit system;
-        config.allowUnfree = true;
-        overlays = [ self.overlays.default ];
-      };
-
-      packages.default = pkgs.rx-nvim;
-    };
+{ lib, ... }:
+final: prev:
+lib.packagesFromDirectoryRecursive {
+  callPackage = lib.callPackageWith (
+    prev.pkgs
+    // {
+      inherit prev;
+      sources = final.callPackage ./_sources/generated.nix { };
+    }
+  );
+  directory = ./.;
 }
