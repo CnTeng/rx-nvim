9a678aa904cbcff28e45fc20b016894ece42b7b6
CnTeng <rxsnakepi@gmail.com>
2025-12-07 01:07:46 +0800

    feat: change fold method for git log

--- config/lua/git/fugitive.lua
+++ config/lua/git/fugitive.lua
@@ -8,6 +8,14 @@ return {
       { "<leader>gd", "<cmd>aboveleft Gvdiffsplit<cr>", desc = "Diff" },
       { "<leader>gD", "<cmd>aboveleft Gvdiffsplit origin<cr>", desc = "Diff (cached)" },
     },
+    init = function()
+      vim.api.nvim_create_autocmd("User", {
+        pattern = "FugitiveCommit",
+        callback = function()
+          vim.opt_local.foldmethod = "syntax"
+        end,
+      })
+    end,
   },
 
   {
