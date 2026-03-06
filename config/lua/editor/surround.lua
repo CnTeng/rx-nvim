9889097ebbf909219b29e263a6511fa051044863
CnTeng <rxsnakepi@gmail.com>
2026-03-06 21:54:44 +0800

    feat: migrate nvim-surround

--- config/lua/editor/surround.lua
+++ config/lua/editor/surround.lua
@@ -2,19 +2,4 @@
 return {
   "kylechui/nvim-surround",
   event = "LazyFile",
-  opts = {
-    keymaps = {
-      insert = "<C-g>s",
-      insert_line = "<C-g>S",
-      normal = "gs",
-      normal_cur = "gss",
-      normal_line = "gS",
-      normal_cur_line = "gSS",
-      visual = "gs",
-      visual_line = "gS",
-      delete = "gsd",
-      change = "gsc",
-      change_line = "gSc",
-    },
-  },
 }
